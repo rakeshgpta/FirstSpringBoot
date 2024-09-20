16d3e6349720d6b3ce2259020a003704153a6344
rakesh_gupta <rakeshkatrisarai@gmail.com>
2024-09-20 07:56:45 +0530

    first commit

---
+++ src/main/java/com/rakesh/DevOpsIntegration/App.java
@@ -0,0 +1,14 @@
+package com.rakesh.DevOpsIntegration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("My First Devops Integration");
+    }
+}
